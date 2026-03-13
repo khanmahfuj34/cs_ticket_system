f2d254835d330c2af289e40021b999ba45a3ce49
khanmahfuj34 <mkmahfujkhanms3@gmail.com>
2026-03-13 20:36:30 +0600

    update resolved task

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -8,24 +8,34 @@ import TicketCard from './component/TicketCard'
 import ticketsData from './data/ticketdata.json'
 
 
+
 function App() {
+  const [ticket, setTicket] = useState(ticketsData);
   const [selectedTask, setSelectedTask]=useState([]);
+  const[resolvedTask, setResolvedTask]=useState([]);
   const handelAddTask = (ticket)=>{
     const alreadyExists = selectedTask.find((item) => item.id===ticket.id);
     if (alreadyExists){
-      toast.error('This ticket is already Added');
-      console.log('This ticket')
+      // alert('This ticket is already Added');
+      // console.log('This ticket')
       return;
     }
     setSelectedTask([...selectedTask, ticket]);
-    toast.success('Ticket added to task status')
-    console.log("ticket added")
+    // alert('Ticket added to task status');
+    // console.log("ticket added")
   }
+  const handelCompleteTask = (task) =>{
+    const remainingTask = selectedTask.filter((item)=>item.id !== task.id);
+    setSelectedTask(remainingTask);
+    setResolvedTask([...resolvedTask, task]);
+    const remainingTicket = ticket.filter((item)=>item.id !== task.id);
+    setTicket(remainingTicket);
+  };
 
   return (
     <>
      <Navbar />
-     <Banner inProgressCount={selectedTask.length} resolvedCount={0}></Banner>
+     <Banner inProgressCount={selectedTask.length} resolvedCount={resolvedTask.length}></Banner>
      
     <div className='w-11/12 mx-auto mt-8 grid grid-cols-1 lg:grid-cols-3 gap-6'>
       <div className="lg:col-span-2">
@@ -37,7 +47,7 @@ function App() {
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          {ticketsData.map((ticket) => (
+          {ticket.map((ticket) => (
             <TicketCard key={ticket.id}
             ticket={ticket}
             handelAddTask={handelAddTask}
@@ -45,20 +55,54 @@ function App() {
           ))}
         </div>
       </div>
+      {/* task status */}
       <div className="bg-white rounded-2xl p-5 border border-gray-200 min-h-[400px]">
-        <div className="pb-4 border-b border-gray-100">
           <h2 className="text-2xl font-bold text-[#0F172A]">Task Status</h2>
-          <p className="text-sm text-gray-500 mt-1">
+          <p className="text-sm text-gray-500 mt-1 mb-4">
             Selected tickets will appear here
           </p>
-        </div>
 
-        <div className="py-8 flex items-center justify-center">
-          <p className="text-sm text-gray-400">No task selected yet</p>
-        </div>
+          {selectedTask.length === 0 ? (
+            <p className="text-sm text-gray-400 mt-6">No task selected yet</p>
+          ) : (
+            <div className="mt-5 space-y-4">
+              {selectedTask.map((task) => (
+                <div
+                  key={task.id}
+                  className="bg-[#F5F7FF] border border-[#E5E7EB] rounded-xl p-4"
+                >
+                  <h3 className="text-sm font-semibold text-[#0F172A]">
+                    {task.title}
+                  </h3>
+                  <button onClick={()=> handelCompleteTask(task)} className='w-full py-2 rounded-lg bg-[#10B981] text-white font-medium'>Complete</button>
+                </div>
+              ))}
+            </div>
+          )}
+          <div className="mt-6">
+        <h3 className="text-xl font-bold text-[#0F172A] mb-3">Resolved Task</h3>
+
+        {resolvedTask.length === 0 ? (
+          <p className="text-sm text-gray-400">No resolved task yet</p>
+        ) : (
+          <div className="space-y-3">
+            {resolvedTask.map((task) => (
+              <div
+                key={task.id}
+                className="bg-[#F1F5FF] border border-[#E5E7EB] rounded-xl p-4"
+              >
+                <p className="text-sm font-medium text-[#0F172A]">{task.title}</p>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
+      </div>
+      {/* Resolve Task */}
+      
+      
     </div>
-    <ToastContainer></ToastContainer>
+    
     </>
   )
 }
--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+
 import './index.css'
 import App from './App.jsx'
 document.documentElement.setAttribute('data-theme', 'light')
@@ -9,6 +8,6 @@ document.documentElement.setAttribute('data-theme', 'light')
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <ToastContainer />
+    
   </StrictMode>,
 )
